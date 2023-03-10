867d6808977997cc703353cb819483cb946d0162
sokolovaeldm <sokolova.el.dm@gmail.com>
2023-03-10 12:22:59 +0100

    add CORS policy

--- ShoppingCartAPI/Program.cs
+++ ShoppingCartAPI/Program.cs
@@ -4,9 +4,21 @@ using ShoppingCartAPI;
 using ShoppingCartAPI.Data;
 using ShoppingCartAPI.Services;
 
+
+var ShoppingCartCORSPolicy = "_ShoppingCartCORSPolicy";
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: ShoppingCartCORSPolicy,
+        policy =>
+        {
+            policy.WithOrigins("*");
+            policy.WithHeaders("*");
+            policy.WithMethods("*");
+        });
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,10 +29,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<MyDbContext>(options =>
 {
 
-    var hostname = Environment.GetEnvironmentVariable("RDS_HOSTNAME");
-    var connectionString = $"Host={hostname};Username=academyadmin;Password=shWWq44NEEVuSLh;Database=ebdb";
+    //var hostname = Environment.GetEnvironmentVariable("RDS_HOSTNAME");
+    //var connectionString = $"Host={hostname};Username=academyadmin;Password=shWWq44NEEVuSLh;Database=ebdb";
 
-    //var connectionString = "Host=localhost;Username=postgres;Password=123456;Database=postgres";
+    var connectionString = "Host=localhost;Username=postgres;Password=123456;Database=postgres";
     options.UseNpgsql(connectionString);
 });
 builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartEntityRepository>();
@@ -34,35 +46,35 @@ var app = builder.Build();
 
 app.CreateDbIfNotExists();
 
+app.UseCors();
+
 app.MapGet("/", () => "Hello World!");
+
 
 app.MapGet("/products", (ProductService productService) =>
 {
     var availableProducts = productService.GetAvailableProducts();
     var options = new JsonSerializerOptions { WriteIndented = true };
     return JsonSerializer.Serialize(availableProducts, options);
-});
-
-app.MapGet("/host-name", () => Environment.GetEnvironmentVariable("RDS_HOSTNAME"));
+}).RequireCors(ShoppingCartCORSPolicy);
 
 app.MapPost("/add-item", (IShoppingCartService shoppingCartService, ItemRequest request) =>
 {
     shoppingCartService.Add(request.ProductName);
     return $"Item {request.ProductName} added";
-});
+}).RequireCors(ShoppingCartCORSPolicy);
 
 app.MapGet("/shopping-cart", (IShoppingCartService shoppingCartService) =>
 {
     var shoppingCart = shoppingCartService.GetShoppingCart();
     var options = new JsonSerializerOptions { WriteIndented = true };
     return JsonSerializer.Serialize(shoppingCart, options);
-});
+}).RequireCors(ShoppingCartCORSPolicy);
 
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
-
 
 
 app.UseHttpsRedirection();
